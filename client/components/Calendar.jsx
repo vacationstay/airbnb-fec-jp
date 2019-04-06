a4b1417118db52ff95e1b62e7d2f633f15637620
Julian Perez <juuprz@gmail.com>
2019-04-06 14:40:10 -0700

    Remove unused code in Calendar component

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { CalendarHeader } from './CalenderHeader.jsx';
 
 function Calendar(props) {
-  return (
-    <div className={props.hideClass}>
-      <div className={props.calendar.length === 6 ? 'calContainer calXL' : 'calContainer'}>
-        <div >
-          <CalendarHeader buildCalendarRowForSelectedMonth={props.buildCalendarRowForSelectedMonth} determineStructureOfCalendarForSelectedMonth={props.determineStructureOfCalendarForSelectedMonth} onCalendarChangeClick={props.onCalendarChangeClick} selectDates={props.selectDates}/>
-        </div>
-        <div className='calInner'>
-          {props.calendar}
-        </div>
-      </div>
-    </div>
-  )
+  // return (
+    // <div className={props.hideClass}>
+    //   <div className={props.calendar.length === 6 ? 'calContainer calXL' : 'calContainer'}>
+    //     <div >
+    //       <CalendarHeader buildCalendarRowForSelectedMonth={props.buildCalendarRowForSelectedMonth} determineStructureOfCalendarForSelectedMonth={props.determineStructureOfCalendarForSelectedMonth} onCalendarChangeClick={props.onCalendarChangeClick} selectDates={props.selectDates}/>
+    //     </div>
+    //     <div className='calInner'>
+    //       {props.calendar}
+    //     </div>
+    //   </div>
+    // </div>
+  // )
 }
 
 export {
